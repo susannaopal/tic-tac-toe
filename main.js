846fb46b2b524063268801de274b2c02206f01a3
Susanna Carey <susannaopal@gmail.com>
2021-09-25 11:51:14 -0400

    Update class files with basic JS functions and set classes, include basic pseudocode notes

---
+++ main.js
@@ -0,0 +1,22 @@
+// A main.js file that contains all DOM related JavaScript
+
+//may need to connect the game and player class files here
+//instiatiate the classes here
+//review ROMCOM < instite the new book
+
+//everything needs to go in here
+
+//build out HTML/css stuff
+//PSEUDOCODE MORE after getting to display
+//then go through the user flow
+//when opening the page what should i see?
+//instiate 2 new players on the page load
+//conditionals for each players takeTurn
+//evaluate a turn
+
+//can skip a step to keep working around until getting to localStorage
+//do not worry about this yet
+
+//eventlisterers and queries here to see what needs to be selected and when
+//functionality will come from here
+//WRITE LITTLE LITTLE CHUNKS
