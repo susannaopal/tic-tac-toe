b38aa5819e8694f18ef4beb1139dff79a92fe532
Susanna Carey <susannaopal@gmail.com>
2021-09-26 22:18:30 -0400

    Update player class to run method better

--- player.js
+++ player.js
@@ -20,12 +20,13 @@ class Player {
     this.token = token;
     //if token = this class attribute then run this or this instead
     //the token could take a string or am emoji
-    this.numberOfWins = [];
+    // this.numberOfWins = [];
     //ultimately this below goes for local storage counting of wins
     this.counterOfWins = 0;
+    //^^ use this for localStorage
     //project spec shows an empty array??
     this.winner = false;
-    this.draw = false;
+    // this.draw = false;
   }
   saveWinsToStorage(){
   }
